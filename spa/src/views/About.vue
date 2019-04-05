1f76bca44964ee8470875207502ee05a2e7762e4
Mark Harder <markh@stratagen.local>
2019-04-04 22:18:31 -0700

    full lesson

--- spa/src/views/About.vue
+++ spa/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button v-on:click="goHome">Back Home</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'about',
+  methods: {
+    goHome: function() {
+      // named route using an object
+      this.$router.push({ name: 'home' });
+    }
+  }
+}
+</script>
